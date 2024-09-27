e78b69612bdf9f4e4ec5ed1fe8133fb2fbe46397
Anthony Meek <meekyam37@gmail.com>
2024-09-27 12:47:30 +0100

    Replacing the layout file and using embedded Ruby for titles across static pages (3.4.3)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -16,7 +17,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
-
 
   test "should get about" do
     get static_pages_about_url
