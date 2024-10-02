7a11706c9989f51b0cfc5d5fb2aab02f71c14150
Anthony Meek <meekyam37@gmail.com>
2024-10-02 17:02:54 +0100

    Adding require / permit user_params (7.3.2)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
     else
@@ -17,5 +17,10 @@ class UsersController < ApplicationController
     end
   end
 
-  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
