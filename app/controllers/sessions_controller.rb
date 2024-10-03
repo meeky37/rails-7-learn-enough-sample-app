d72538d647bc4e9cc3c279dcd89924eca390d30d
Anthony Meek <meekyam37@gmail.com>
2024-10-03 15:27:54 +0100

    Include log_in method in create session (post) (8.2.1)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,14 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
       # Create an error message
       flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
     end
-    render 'new', status: :unprocessable_entity
   end
 
   def destroy
