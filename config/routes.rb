1e43179fd9c3dc3ca457a1c128ee3aee2d9cf91b
Anthony Meek <meekyam37@gmail.com>
2024-10-03 11:15:36 +0100

    Add Sessions controller (8.1.1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root "static_pages#home" # removed the route for 'static_pages/home', as we’ll always use root_path or root_url instead.
   get  "/help",    to: "static_pages#help"
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get  "/signup", to: "users#new"
+  get  "/login", to: "sessions#new"
+  post  "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
 
