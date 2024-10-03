079a8ac3eb119f7e2644697c3272cee768caccf7
Anthony Meek <meekyam37@gmail.com>
2024-10-03 15:11:27 +0100

    Add a flash test and make the flash a flash.now so it disappears appropriately (8.1.5)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to user's show page.
     else
       # Create an error message
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
     end
     render 'new', status: :unprocessable_entity
   end
