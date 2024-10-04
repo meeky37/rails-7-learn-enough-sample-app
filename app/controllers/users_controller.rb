55c86f50a8ecf0715cfad127d8ae7ce637f10897
Anthony Meek <meekyam37@gmail.com>
2024-10-04 16:42:58 +0100

    Add user site to update their details - implemented functionality to follow (10.1.1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
-  end  
-  
+  end
+
   def new
     @user = User.new
   end
@@ -20,10 +20,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-
 end
