e8b3052dc78c2558f6131d44a9d584e102affad9
Anthony Meek <meekyam37@gmail.com>
2024-10-04 12:47:45 +0100

    Add remember functionality upon login (9.1.2)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,8 +6,16 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any).
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+
+    # If session of user id exists (while setting user id to session of user id)…
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -19,6 +27,14 @@ module SessionsHelper
   def log_out
     reset_session
     @current_user = nil
+  end
+
+
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
 end 
