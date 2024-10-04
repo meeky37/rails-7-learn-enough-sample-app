8d2c5860b1d4f179636cfc0fe78f0e76f8735d34
Anthony Meek <meekyam37@gmail.com>
2024-10-04 16:07:45 +0100

    Add session replay protection (9.3.2)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,17 @@ module SessionsHelper
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
+    # Guard against session replay attacks.
+     # See https://bit.ly/33UvK0w for more.
+     session[:session_token] = user.session_token
+  end
+
+
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   # Returns the current logged-in user (if any).
@@ -9,7 +20,10 @@ module SessionsHelper
 
     # If session of user id exists (while setting user id to session of user id)…
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -35,13 +49,4 @@ module SessionsHelper
     reset_session
     @current_user = nil
   end
-
-
-  # Remembers a user in a persistent session
-  def remember(user)
-    user.remember
-    cookies.permanent.encrypted[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,9 +21,17 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  # Returns a session token to prevent session hijacking.
+  # We reuse the remember digest for convenience.
+  def session_token
+    remember_digest || remember
   end
 
   # Returns true if the given token matches the digest.
