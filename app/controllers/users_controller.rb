70a63ed4276d79f88f00a96185032b83b0392052
Anthony Meek <meekyam37@gmail.com>
2024-10-07 10:47:50 +0100

    Add fixture, tests and additional before_action so users are only allowed to edit their own info (10.2.2)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -52,4 +53,9 @@ class UsersController < ApplicationController
       end
     end
 
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless @user == current_user
+    end
 end
