a1050858505f81b50aefe6dcca469b090f265b21
Anthony Meek <meekyam37@gmail.com>
2024-10-07 14:06:22 +0100

    Update layout testing to cover logged in scenarios (10.3.1)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,6 +15,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     get contact_path
     assert_select "title", full_title("Contact")
+    
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get about_path
+    assert_select "title", full_title("About")
+
   end
   #   assert true
   # end
