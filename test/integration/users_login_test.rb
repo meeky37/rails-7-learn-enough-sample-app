a3540a4cc2e61a588c344120db6c0ffcec9133e6
Anthony Meek <meekyam37@gmail.com>
2024-10-09 10:02:29 +0100

    Refactor tests for user login (11.3.3)

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,19 +1,22 @@
 require "test_helper"
 
 class UsersLogin < ActionDispatch::IntegrationTest
+
   def setup
     @user = users(:michael)
   end
 end
 
 class InvalidPasswordTest < UsersLogin
+
   test "login path" do
     get login_path
     assert_template 'sessions/new'
   end
 
   test "login with valid email/invalid password" do
-    post login_path, params: { session: { email: @user.email, password: "invalid" }}
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
     assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
@@ -24,12 +27,12 @@ class InvalidPasswordTest < UsersLogin
 end
 
 class ValidLogin < UsersLogin
-  
+
   def setup
     super
-    post login_path, params: { session: { email: @user.email, password: 'password'}}
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
   end
-
 end
 
 class ValidLoginTest < ValidLogin
@@ -38,7 +41,8 @@ class ValidLoginTest < ValidLogin
     assert is_logged_in?
     assert_redirected_to @user
   end
-  test "redirect following login" do
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
@@ -48,12 +52,16 @@ class ValidLoginTest < ValidLogin
 end
 
 class Logout < ValidLogin
+
   def setup
     super
     delete logout_path
   end
+end
 
-  test "logout successful" do
+class LogoutTest < Logout
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
@@ -62,13 +70,10 @@ class Logout < ValidLogin
   test "redirect after logout" do
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
-   end
-end
+  end
 
-class LogoutTest < Logout
-  
   test "should still work after logout in second window" do
     delete logout_path
     assert_redirected_to root_url
@@ -76,11 +81,11 @@ class LogoutTest < Logout
 end
 
 class RememberingTest < UsersLogin
-  
+
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
     assert_not cookies[:remember_token].blank?
-    assert_equal cookies[:remember_token], assigns(:user).remember_token 
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
   end
 
   test "login without remembering" do
