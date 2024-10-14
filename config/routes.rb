332b414e17d43bf27c9b5cdd0c03067299ae2f80
Anthony Meek <meekyam37@gmail.com>
2024-10-14 16:27:25 +0100

    Add a proto-feed (13.3.3)
    - Add a feed instance variable to the home action
    
    - Add a status feed partial
    
    - Add a status feed to the Home page
    
    - Add an (empty) @feed_items instance variable to the create action
    
    - Set an explicit controller and action feed partial to fix routing error
    
    - Fix a refresh error in some browsers using /microposts iin routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: %i[new create edit update]
   resources :microposts, only: %i[create destroy]
+  get 'microposts', to: 'static_pages#home'
 end
 
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
