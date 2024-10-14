66c1626c03266a799befb2f93d55795b5d4773dc
Anthony Meek <meekyam37@gmail.com>
2024-10-14 10:18:39 +0100

    Refine microposts with reverse order display and destroy with user delete (13.1.4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,25 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
-  validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-  format: { with: VALID_EMAIL_REGEX },
-  uniqueness: true
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-
-# Returns the hash digest of a string
+  # Returns the hash digest of a string
   def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                              BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost:)
   end
 
   def self.new_token
@@ -40,10 +43,11 @@ class User < ApplicationRecord
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
+
     BCrypt::Password.new(digest).is_password?(token)
   end
 
- # Forgets a user
+  # Forgets a user
   def forget
     update_attribute(:remember_digest, nil)
   end
@@ -83,7 +87,7 @@ class User < ApplicationRecord
 
   # Creates and assigns the activation token and digest.
   def create_activation_digest
-    self.activation_token  = User.new_token
+    self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
 end
