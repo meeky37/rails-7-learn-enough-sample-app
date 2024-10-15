2654fdd6c2549ca4d80b108827658692a282c7c3
Anthony Meek <meekyam37@gmail.com>
2024-10-15 13:20:29 +0100

    Add sample following data (14.2.1)

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,11 @@ users = User.order(:created_at).take(6)
   ].sample
   users.each { |user| user.microposts.create!(content:) }
 end
+
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
