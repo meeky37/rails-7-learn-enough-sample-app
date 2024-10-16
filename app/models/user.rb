7db2948410fe2c8bb7b1a154563231a9b85119b8
Anthony Meek <meekyam37@gmail.com>
2024-10-16 09:15:33 +0100

    Add an initial feed implementation (14.3.2) - New def feed action returns micropost where user_id is in following_ids or is their own id

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,10 +86,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # Defines a proto-feed.
-  # See "Following users" for the full implemenation.
+  # Returns a user's status feed.
   def feed
-    Micropost.where('user_id = ?', id)
+    Micropost.where('user_id IN (?) OR user_id = ?', following_ids, id)
+    # Micropost.all - Exercise to break test
   end
 
   # Follows a user.
